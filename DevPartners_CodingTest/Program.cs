95dc59fa74e1929ae5895eafc2099d9ce77e33e5
Jason Germata <jsn.mastermind@gmail.com>
2024-04-17 00:36:35 +0800

    follow Api Specification on provided schema

--- DevPartners_CodingTest/Program.cs
+++ DevPartners_CodingTest/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add HttpClient Service.
@@ -8,21 +10,42 @@ builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Rainfall Api",
+        Version = "v1",
+        Description = "An API which provides rainfall data",
+        Contact = new OpenApiContact
+        {
+            Name = "Sorted",
+            Url = new Uri("https://www.sorted.com"),
+        },
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rainfall Api v1");
+});
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    app.MapControllers();
+});
+
 
 app.Run();
