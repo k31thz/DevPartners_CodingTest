e4ee36000e265854c4abc7131ac5cf7551513dfa
Jason Germata <jsn.mastermind@gmail.com>
2024-04-16 21:47:46 +0800

    add Contoller file and Model for Rainfall measurement (no logical contents)

---
+++ DevPartners_CodingTest/Controllers/RainfallController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevPartners_CodingTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RainfallController : ControllerBase
+    {
+        // GET: api/<RainfallController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ DevPartners_CodingTest/Models/Rainfall.cs
@@ -0,0 +1,10 @@
+﻿namespace DevPartners_CodingTest.Models
+{
+    public class Rainfall
+    {
+        public string Id { get; set; }
+        public string dateTime { get; set; }
+        public string measure { get; set; }
+        public double value { get; set }
+    }
+}
